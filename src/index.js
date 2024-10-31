7f7bc1520d8c4b5eb42bfa8949dcd56e6cab755f
kacper-echaust <k9echaust@gmail.com>
2024-10-31 14:58:18 +0100

    setup telegram

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
+const { Telegraf } = require('telegraf')
+require('dotenv').config()
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.reply('Welcome to your AI Assistant!'));
+bot.start(ctx => ctx.reply('Welcome to your AI Assistant!'))
 
-bot.launch();
+bot.on('message', ctx => {
+	ctx.reply('Napisałeś mi: ' + ctx.message.text)
+})
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+bot.launch()
+
+console.log('Bot started')
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
